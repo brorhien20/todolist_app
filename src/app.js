d7d0e4a16a06855d96dd97a229367984e775f226
Guriase Isaac Oghenebrorhien <48118387+brorhien20@users.noreply.github.com>
2019-10-09 17:19:18 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,60 +1,75 @@
-const store = [
+const createTodoInputSubmit = document.querySelector('[data-dom=createTodoInputSubmit]')
+const createTodoInputBox = document.querySelector('[data-dom=createTodoInputBox]')
+const todolist = document.querySelector('[data-dom=todolist]')
 
-]
+const store = {
+    todos: {
+        '_id1100': { value: 'Wash some stuffs', isDone: false }
+    },
+    isEditing: null
+}
 
-const app = document.querySelector('#app')
+render()
 
-const createTodo = () => {
-    if (inputElemText.value.length > 0) { 
-        const FilteredTodos = store.filter(todo => todo.title === inputElemText.value)
-        if (FilteredTodos.length === 0){
-            store.push({
-                title: inputElemText.value,
-                isDone: false
-            })
+const generateId = () => {
+    return '_id'.concat(Math.round(Math.random() * new Date().getTime()))
+}
+
+function render() {
+    const { todos } = store
+    const alreadyAddedtodos = Array
+        .from(todolist.children)
+        .map(child => child.getAttribute('data-dom-todoId'))
+
+
+    for (const key of Object.keys(todos)) {
+        if (alreadyAddedtodos.includes(key)) continue
+        const { value, isDone } = todos[key]
+        todolist.innerHTML = `<li class='${isDone ? 'done' : ''}' data-dom-todoID=${key} >
+        <span data-dom='todoItem'>${value}</span>
+        <button data-dom='deleteTodoItem'>Delete</button>
+        <button data-dom='updateTodoItem'>Update</button>
+        </li>` + todolist.innerHTML
+    }
+}
+
+createTodoInputSubmit.addEventListener('click', (event) => {
+    event.stopPropagation()
+    const { value } = createTodoInputBox;
+    const { todos, isEditing } = store
+    if (value) {
+        if (isEditing === null) {
+            todos[generateId()] = { value, isDone: false }
+            createTodoInputBox.value = ""
+            render()
+        } else {
+            const { isDone } = todos[isEditing] || false
+            todos[isEditing] = { value, isDone }
+            document.querySelector(`[data-dom-todoId=${isEditing}]`).remove()
+            createTodoInputBox.value = ''
+            store.isEditing = null
+            render()
         }
     }
-    const todoList = BuildElement('div', [createList(store)],'showTodo')
-    const mainWrapper = BuildElement('div', [createTodoWrapper,todoList])
-    app.appendChild(mainWrapper)
-}
 
-const BuildElement = (element, children=[], className, style) => {
-    const newElement = document.createElement(element)
-    if (className) newElement.classList.add(className)
-    if (style) newElement.style = style
-    if (children.length > 0) children.forEach(child => newElement.appendChild(child))
-    return newElement
-}
+})
+ 
 
-const createInput = (type='text', value='',className, style) => {
-    const input = document.createElement('input')
-    if (className) input.classList.add(className)
-    if (style) input.style = style
-    input.type = type
-    input.value = type === 'submit' && !value ? type : value
-    return input
-}
-
-const createList = (list=[]) => {
-    const ul = document.createElement('ul')
-    list.forEach(el => {
-        const li = document.createElement('li')
-        li.innerText = el.title
-        ul.appendChild(li)
-    })
-    return ul
-}
-
-const inputElemText = createInput()
-const inputElemSubmit = createInput('submit','Add Todo')
-inputElemSubmit.addEventListener('click', createTodo)
-
-const createTodoWrapper = BuildElement('div', [inputElemText, inputElemSubmit],'createTodo')
-const todoList = BuildElement('div', [createList(store)],'showTodo')
-
-const mainWrapper = BuildElement('div', [createTodoWrapper,todoList])
-
-app.appendChild(mainWrapper)
-
-console.log(app)
+todolist.addEventListener('click', event => {
+    event.stopPropagation()
+    const parent = event.target.parentElement
+    const id = parent.getAttribute('data-dom-todoId')
+    const { todos } = store
+    if (event.target.getAttribute('data-dom') === 'deleteTodoItem') {
+        parent.remove()
+        delete todos[id]
+    }
+    if (event.target.getAttribute('data-dom') === 'updateTodoItem') {
+        createTodoInputBox.value = todos[id] ? todos[id].value : parent.children[0].innerText
+        store.isEditing = id
+    }
+    if (event.target.getAttribute('data-dom') === 'todoItem') {
+        parent.classList.toggle('done')
+        if (todos[id]) todos[id] = {value: todos[id].value, isDone: !todos[id].isDone} 
+    }
+})
